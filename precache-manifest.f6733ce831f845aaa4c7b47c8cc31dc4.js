1e4435be30b1e2a76198b8dc9ca1aef4ad497fdc
Vincent Monard <vincent.monard@free.fr>
2020-04-30 03:02:23 +0200

    Big refactoring, updated packages, improved design, edited colors. To be done : refresh data, improve project view

---
+++ precache-manifest.f6733ce831f845aaa4c7b47c8cc31dc4.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f783077b09ffa8bf8af4ddff208145c1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b8b1d2da38629041bd88",
+    "url": "/static/js/0.70c17df6.chunk.js"
+  },
+  {
+    "revision": "3d99d251d16f46790842",
+    "url": "/static/js/3.52c452fb.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/3.52c452fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0fcb60a1abc2ff50044e",
+    "url": "/static/js/4.7688a46f.chunk.js"
+  },
+  {
+    "revision": "92d470b91d67a2e6958b",
+    "url": "/static/js/5.5ac7922f.chunk.js"
+  },
+  {
+    "revision": "d6d822fc1e69edbf7054",
+    "url": "/static/js/6.5f04dd50.chunk.js"
+  },
+  {
+    "revision": "d96e4eeb600192d31151",
+    "url": "/static/js/main.111e135d.chunk.js"
+  },
+  {
+    "revision": "4b45aaf961eff8779b96",
+    "url": "/static/js/runtime-main.f6a44e67.js"
+  },
+  {
+    "revision": "a3755efbae057e44582a64c1626fdd82",
+    "url": "/static/media/author_picture.a3755efb.png"
+  },
+  {
+    "revision": "3aa82718164f9453c1fddbe6e3f08bbf",
+    "url": "/static/media/ecarlate_banner.3aa82718.png"
+  },
+  {
+    "revision": "ca105e9f94728475fa58204e67080fdc",
+    "url": "/static/media/ecarlate_logo.ca105e9f.png"
+  },
+  {
+    "revision": "34f98bca4693fc379d053f3888a725c0",
+    "url": "/static/media/lucy_banner.34f98bca.png"
+  },
+  {
+    "revision": "4cc4ec4f344b5af180d9836d879fd551",
+    "url": "/static/media/lucy_logo.4cc4ec4f.png"
+  },
+  {
+    "revision": "a261d24f7fe1a730ab4286180ff28c55",
+    "url": "/static/media/peche_banner.a261d24f.png"
+  },
+  {
+    "revision": "aee3bd07fa12cc803c3a03626bfb993b",
+    "url": "/static/media/peche_logo.aee3bd07.png"
+  },
+  {
+    "revision": "870033c19066c08c7dc9c42007c2ca6d",
+    "url": "/static/media/screenshot_ecarlate_1.870033c1.png"
+  },
+  {
+    "revision": "f87b430d38a5ba847549d1d76806dfe2",
+    "url": "/static/media/screenshot_ecarlate_2.f87b430d.png"
+  },
+  {
+    "revision": "cab30d62c42d042946b9455273dbe968",
+    "url": "/static/media/screenshot_ecarlate_3.cab30d62.png"
+  },
+  {
+    "revision": "84d2574c7cb494a63a19a1563b0fbf34",
+    "url": "/static/media/screenshot_ecarlate_4.84d2574c.png"
+  },
+  {
+    "revision": "f62e73c040e96e209dbf540c1654a85f",
+    "url": "/static/media/screenshot_ecarlate_5.f62e73c0.png"
+  },
+  {
+    "revision": "3deb4a30a39f38760d38d54f9c4db5cd",
+    "url": "/static/media/screenshot_ecarlate_6.3deb4a30.png"
+  },
+  {
+    "revision": "421ffd523f86d6f3f220dfef62a11c4b",
+    "url": "/static/media/screenshot_lucy_1.421ffd52.png"
+  },
+  {
+    "revision": "1f5db1e67ccac2c684dd0bc029020435",
+    "url": "/static/media/screenshot_lucy_2.1f5db1e6.png"
+  },
+  {
+    "revision": "567ef457e99c75366e91476638f7da92",
+    "url": "/static/media/screenshot_peche_1.567ef457.png"
+  },
+  {
+    "revision": "7861eb09fc365f4019510b9ce41d3700",
+    "url": "/static/media/screenshot_peche_2.7861eb09.png"
+  },
+  {
+    "revision": "27085dbc7836e3741045af43e4fd2b70",
+    "url": "/static/media/screenshot_peche_3.27085dbc.png"
+  },
+  {
+    "revision": "2702b712673a53fef511314f727922d3",
+    "url": "/static/media/screenshot_peche_4.2702b712.png"
+  }
+]);
