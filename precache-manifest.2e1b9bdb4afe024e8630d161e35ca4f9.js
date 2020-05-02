f4b18b2b84f8b8f230a8b2718c9f7b2f53cec1f9
Vincent Monard <vincent.monard@free.fr>
2020-05-02 21:22:02 +0200

    Reorganised assets, reintroduced projects, using absolute imports, extracted projects to Projects.jsx

---
+++ precache-manifest.2e1b9bdb4afe024e8630d161e35ca4f9.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef86d094d9856cb171c6578901e27cbf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f67674cbf876009b1b35",
+    "url": "/static/js/2.02f4613f.chunk.js"
+  },
+  {
+    "revision": "eac4cf64bf573541fd6dba29c78393a0",
+    "url": "/static/js/2.02f4613f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4710c32dfdaf8deb138",
+    "url": "/static/js/3.887db2b7.chunk.js"
+  },
+  {
+    "revision": "42f7be04e44f9df06ab1",
+    "url": "/static/js/4.c255157b.chunk.js"
+  },
+  {
+    "revision": "b50da5b54c0c4955303b",
+    "url": "/static/js/5.870e7325.chunk.js"
+  },
+  {
+    "revision": "feb227974892d781bb22",
+    "url": "/static/js/main.3d040196.chunk.js"
+  },
+  {
+    "revision": "8ac58c1d341ee479ff5e",
+    "url": "/static/js/runtime-main.7381d09f.js"
+  },
+  {
+    "revision": "5fd63248db18c8e103fbc684ab2cd37c",
+    "url": "/static/media/author_picture.5fd63248.jpg"
+  },
+  {
+    "revision": "3aa82718164f9453c1fddbe6e3f08bbf",
+    "url": "/static/media/ecarlate_banner.3aa82718.png"
+  },
+  {
+    "revision": "ca105e9f94728475fa58204e67080fdc",
+    "url": "/static/media/ecarlate_logo.ca105e9f.png"
+  },
+  {
+    "revision": "34f98bca4693fc379d053f3888a725c0",
+    "url": "/static/media/lucy_banner.34f98bca.png"
+  },
+  {
+    "revision": "4cc4ec4f344b5af180d9836d879fd551",
+    "url": "/static/media/lucy_logo.4cc4ec4f.png"
+  },
+  {
+    "revision": "a261d24f7fe1a730ab4286180ff28c55",
+    "url": "/static/media/peche_banner.a261d24f.png"
+  },
+  {
+    "revision": "aee3bd07fa12cc803c3a03626bfb993b",
+    "url": "/static/media/peche_logo.aee3bd07.png"
+  },
+  {
+    "revision": "870033c19066c08c7dc9c42007c2ca6d",
+    "url": "/static/media/screenshot_ecarlate_1.870033c1.png"
+  },
+  {
+    "revision": "f87b430d38a5ba847549d1d76806dfe2",
+    "url": "/static/media/screenshot_ecarlate_2.f87b430d.png"
+  },
+  {
+    "revision": "cab30d62c42d042946b9455273dbe968",
+    "url": "/static/media/screenshot_ecarlate_3.cab30d62.png"
+  },
+  {
+    "revision": "84d2574c7cb494a63a19a1563b0fbf34",
+    "url": "/static/media/screenshot_ecarlate_4.84d2574c.png"
+  },
+  {
+    "revision": "f62e73c040e96e209dbf540c1654a85f",
+    "url": "/static/media/screenshot_ecarlate_5.f62e73c0.png"
+  },
+  {
+    "revision": "3deb4a30a39f38760d38d54f9c4db5cd",
+    "url": "/static/media/screenshot_ecarlate_6.3deb4a30.png"
+  },
+  {
+    "revision": "421ffd523f86d6f3f220dfef62a11c4b",
+    "url": "/static/media/screenshot_lucy_1.421ffd52.png"
+  },
+  {
+    "revision": "1f5db1e67ccac2c684dd0bc029020435",
+    "url": "/static/media/screenshot_lucy_2.1f5db1e6.png"
+  },
+  {
+    "revision": "567ef457e99c75366e91476638f7da92",
+    "url": "/static/media/screenshot_peche_1.567ef457.png"
+  },
+  {
+    "revision": "7861eb09fc365f4019510b9ce41d3700",
+    "url": "/static/media/screenshot_peche_2.7861eb09.png"
+  },
+  {
+    "revision": "27085dbc7836e3741045af43e4fd2b70",
+    "url": "/static/media/screenshot_peche_3.27085dbc.png"
+  },
+  {
+    "revision": "2702b712673a53fef511314f727922d3",
+    "url": "/static/media/screenshot_peche_4.2702b712.png"
+  }
+]);
